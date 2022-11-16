9eb072e719cc65800931dea4819c7d377947d95a
crxtzonceu <zamanthaquijano@gmail.com>
2022-11-17 06:51:47 +0800

    small changes

--- main.js
+++ main.js
@@ -115,5 +115,4 @@ const resetGame = () => {
   document.getElementById('wins').textContent = 0
   document.getElementById('loss').textContent = 0
   document.getElementById('ties').textContent = 0
-  document.getElementById('reset').style.cssText = 'background: red;font-weight: bold;'
 }

393242840d14ea9fb8786c1ce9c6678ff9f3acfc
crxtzonceu <zamanthaquijano@gmail.com>
2022-11-17 06:13:20 +0800

    Done Program

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const displayRoundResult = document.getElementById('round-message')
 const displayScore = document.getElementById("score")
 let playerScore = 0
 let botScore = 0
+let totalGames = 0
 let wins = 0
 let loss = 0
 let ties = 0
@@ -10,7 +11,9 @@ let ties = 0
 // event listeners
 document.addEventListener('click', function(e){
   // filter the clicks
-  if(playerScore == 5 || botScore == 5) {
+  if (e.target.id == 'reset') {
+    resetGame()
+  } else if(playerScore == 5 || botScore == 5) {
     return
   }
   else if(e.target.id == 'rock') {
@@ -31,23 +34,26 @@ const playRound = playerMove => {
   if(playerMove == botMove) {
     result = "Tie"
     ties++
+    totalGames++
   } else if(playerMove == 'rock' && botMove == 'scissor' ||
           playerMove == 'paper' && botMove == 'rock' ||
           playerMove == 'scissor' && botMove == 'paper') {
     result = 'Win'
     wins++
     playerScore++
+    totalGames++
   } else {
     result = 'Lose'
     loss++
     botScore++
+    totalGames++
   }
   // pass the moves and call to render the dom
   renderHtml(playerMove, botMove, result)
 }
 
 const renderHtml = (p, b, result) => {
-  // Uppercase the Moves
+  // TitleCase the Moves
   const player =  makeFirstLetterUpperCase(p)
   const bot = makeFirstLetterUpperCase(b)
   // Render the Score and the Moves
@@ -57,13 +63,23 @@ const renderHtml = (p, b, result) => {
   VS 
   <span class="space">${bot}</span>:Bot</p>
   `
-  // Round Result
-  if(result == 'Tie'){
-    displayRoundResult.textContent = `It's a Tie!`
+  // Round Message Result
+  if(playerScore == 5 || botScore == 5) {
+    // Check who is the winner
+    if(playerScore > botScore) {
+      displayRoundResult.textContent = "You Won the Game!"
+    } else {
+      displayRoundResult.textContent = "You Lost the Game!"
+    }
   } else {
-    displayRoundResult.textContent = `Round Result: ${result}!`
+    if(result == 'Tie'){
+      displayRoundResult.textContent = `It's a Tie!`
+    } else {
+      displayRoundResult.textContent = `Round Result: ${result}!`
+    }
   }
   // Game Stats
+  document.getElementById('total-games').textContent = totalGames
   document.getElementById('wins').textContent = wins
   document.getElementById('loss').textContent = loss
   document.getElementById('ties').textContent = ties
@@ -82,4 +98,21 @@ const makeFirstLetterUpperCase = word => {
 
 const resetGame = () => {
   // reset the game, variables, and the dom
+  playerScore = 0
+  botScore = 0
+  totalGames = 0
+  wins = 0
+  loss = 0
+  ties = 0
+  displayScore.textContent = `Score : ${playerScore} VS ${botScore}`
+  displayMove.innerHTML = `
+  <p>Player: <span class="space">Waiting</span> 
+  VS 
+  <span class="space">Waiting</span>:Bot</p>
+  `
+  displayRoundResult.textContent = `Waiting for the game to start!`
+  document.getElementById('total-games').textContent = 0
+  document.getElementById('wins').textContent = 0
+  document.getElementById('loss').textContent = 0
+  document.getElementById('ties').textContent = 0
 }
